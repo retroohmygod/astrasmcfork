211f478b52d8cc455a34523157e548587cf5b776
Admin <Admin@DESKTOP-N78GL5F>
2026-03-05 19:28:45 +0000

    add some aether stuff

--- Minecraft.World/Dimension.cpp
+++ Minecraft.World/Dimension.cpp
@@ -10,6 +10,7 @@
 #include "HellDimension.h"
 #include "NormalDimension.h"
 #include "TheEndDimension.h"
+#include "TheAetherDimension.h"
 #include "net.minecraft.world.level.tile.h"
 #include "..\Minecraft.Client\Minecraft.h"
 #include "..\Minecraft.Client\Common\Colours\ColourTable.h"
@@ -191,6 +192,7 @@ Dimension *Dimension::getNew(int id)
 	if (id == -1) return new HellDimension();
 	if (id == 0) return new NormalDimension();
 	if (id == 1) return new TheEndDimension();
+		if (id == 2) return new TheAetherDimension();
 
 	return NULL;
 }
